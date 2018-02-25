5cabf652b8c302cdea71fcdd6ed126394849a808
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-02-25 08:01:51 +0200

    shorter_better

--- poem_fresh.py
+++ poem_fresh.py
@@ -4,8 +4,10 @@ Created on Sat Feb 24 06:48:04 2018
 @author: Oyedayo Oyelowo
 """
 #Getting the document directly online
+from collections import defaultdict
 import pandas as pd
 import numpy as np
+
 
 #READING FROM THE USER DIRECTORY
 POEM_FILEPATH = "C:/Users/oyeda/Desktop/Oyedayo_Oyelowo_verto_data_analyst/poem.txt"
@@ -43,31 +45,22 @@ WORD_LIST = POEM_BODY_LOWERCASE.split()
 LIST_DELTA_TIME = ["{0}: t_{1}".format(w, t) for w, t in zip(WORD_LIST, COST_TIME_LIST)]
 #az = dict((el, h) for el,h in zip(WORD_LIST, COST_TIME_LIST))
 
+#Create a dictionary for each word as the key and their corresponding time_cost
+#as the value
+WORDS_TIME_DICT = defaultdict(list)
+for key, value in zip(WORD_LIST, COST_TIME_LIST):
+    WORDS_TIME_DICT[key].append(value)
+
 
 ##############################################################################
 
 #Create an empty dataframe.
 WORDS_DATA = pd.DataFrame()
-for i, word_in_list in enumerate(WORD_LIST):
-    print("Processing Word....{0}/{1}.".format(i + 1, len(WORD_LIST)))
-    WORDS_DATA.loc[i, 'words'] = word_in_list
-    WORDS_DATA.loc[i, 'count'] = WORD_LIST.count(word_in_list)
-#    get the list of positions where each alphabet occured
-    letter_list = [idx for letter, idx in zip(WORD_LIST, COST_TIME_LIST) if letter == word_in_list]
-    if len(letter_list) > 1:
-        delta_time_list = []
-        for value, letter in enumerate(letter_list):
-            if value + 1 < len(letter_list):
-                time1, time2 = letter_list[value], letter_list[value + 1]
-                delta_time = time2 - time1
-                delta_time_list.append(delta_time)
-            WORDS_DATA.loc[i, 'Average_wt_time'] = np.mean(delta_time_list)
+for i, (key, value) in enumerate(WORDS_TIME_DICT.items()):
+    print('Processing {0}.......{1} out of{2}'.format(key, i+1, len(WORDS_TIME_DICT)))
+    WORDS_DATA.loc[key, 'words'], WORDS_DATA.loc[key, 'count'] = key, len(value)
+    if len(value) < 2:
+        WORDS_DATA.loc[key, 'avg_tm'] = 0
     else:
-        WORDS_DATA.loc[i, 'Average_wt_time'] = 0
-
-GROUPED = WORDS_DATA.groupby('words')
-WORDS_DATA_GROUPED = GROUPED['count', 'Average_wt_time'].agg([np.unique]).reset_index()
-WORDS_DATA_GROUPED.columns = WORDS_DATA_GROUPED.columns.droplevel(1)
-WORDS_DATA_GROUPED = WORDS_DATA_GROUPED.sort_values(by='count', ascending=False)
-WORDS_DATA_GROUPED = WORDS_DATA_GROUPED[0:100].reset_index()
+        WORDS_DATA.loc[key, 'avg_tm'] = np.mean(np.diff(value))
    
